0239c77e016a52dea29a6978107bc61e5730c050
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2025-04-27 15:19:19 +0300

    Fix dependencies and remove Linux-specific Rollup module

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +11,8 @@ export default defineConfig({
   optimizeDeps: {
     include: ['@emoji-mart/react'] // forces Vite to pre-bundle this dependency
   }
-})
+});
+
+
 
 
